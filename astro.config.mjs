fb55232acaa488ee13e012b134b829b4d4874858
aaronamm <theeranitp@gmail.com>
2025-03-31 13:11:45 +0700

    Initial setup with @dotnetthailand/minimal-portfolio-theme

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from "astro/config";
+import MinimalPortforlioTheme from '@dotnetthailand/minimal-portfolio-theme';
+
+export default defineConfig({
+  integrations: [
+    MinimalPortforlioTheme({
+      config: {
+        title: "minimal-portfolio-theme",
+        description: "My awesome theme",
+      },
+      pages: {
+      },
+      overrides: {
+      }
+    }),
+  ],
+});
---
+++ src/content/post/hello.md
@@ -0,0 +1,16 @@
+---
+title: hello 1
+tags:
+  - a
+  - b
+date: '2025-01-01'
+type: blog post
+primaryContributionArea: C#
+additionalContributionAreas:
+  - .NET
+  - Azure
+---
+
+
+
+# Cha ba laa hello in post
